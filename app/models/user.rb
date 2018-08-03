3b56415a9fda9862a490893181af418865aa6157
imWildCat <wildcat.name@gmail.com>
2018-08-03 20:11:13 +0100

    Add default user groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   belongs_to :user_group
+
+  before_create :set_user_group_creation
+
+
+  private
+
+  def set_user_group_creation
+    unless self.user_group_id
+      self.user_group_id = 1
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if UserGroup.count == 0
+  UserGroup.create!([
+                        {
+                            name: 'User'
+                        },
+                        {
+                            name: 'Inactive'
+                        },
+                        {
+                            id: 999,
+                            name: 'Admin'
+                        }
+                    ])
+end
