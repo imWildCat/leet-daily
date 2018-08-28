8234b274697f8da505c97c1ba29c8aadeab09b53
imWildCat <wildcat.name@gmail.com>
2018-08-28 09:11:38 +0100

    Add redis

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,19 @@ module LeetDaily
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.action_controller.perform_caching = true
+    config.cache_store = :redis_cache_store, {:url => 'redis://localhost:6379/0'}
+
+    ActionMailer::Base.smtp_settings = {
+        :user_name => Rails.application.credentials[:sendgrid_username],
+        :password => Rails.application.credentials[:sendgrid_password],
+        :domain => 'leetdaily.com',
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+    }
+
   end
 end
