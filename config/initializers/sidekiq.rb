859879b9013798d5d41e68ea8f14599c7b66558a
imWildCat <wildcat.name@gmail.com>
2018-08-28 17:06:58 +0100

    Add sidekiq schedule

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,9 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://localhost:6379/12' }
 end
+
+schedule_file = 'config/sidekiq_schedule.yml'
+
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
